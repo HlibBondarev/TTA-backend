633071015a2a4404cc1d1a87276a6fa1fa5b0a20
HlibBondarev <Hlib.Bondarev@gmail.com>
2026-03-28 11:52:44 +0200

    fix(code-smell): Added ExcludeFromCodeCoverage attributes to marker class to satisfy static analysis while maintaining assembly scanning functionality.

--- TTA.BusinessLogic/Features/Clubs/Commands/CreateClubCommand.cs
+++ TTA.BusinessLogic/Features/Clubs/Commands/CreateClubCommand.cs
@@ -1,10 +1,12 @@
 ﻿using MediatR;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TTA.BusinessLogic.Features.Clubs.Commands;
 
 /// <summary>
 /// Domain command containing full data needed to create a club.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public record CreateClubCommand(
     string Name,
     Guid CityId,
--- TTA.DataAccess/SqlPlaceholder.cs
+++ TTA.DataAccess/SqlPlaceholder.cs
@@ -1,9 +1,15 @@
-﻿namespace TTA.DataAccess;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TTA.DataAccess;
 
 /// <summary>
 /// Marker class used by DbUp to locate the assembly containing SQL scripts.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public class SqlPlaceholder
 {
     // This class is intentionally left empty.
+    private SqlPlaceholder()
+    {
+    }
 }
